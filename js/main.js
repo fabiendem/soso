de4a3fcfe057e8ca0e3c642e2a527e42e6e4dc52
Fabien Demangeat <fabien.demangeat@orange.com>
2014-09-04 22:34:41 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	// grab an element
+	var header = document.querySelector('header');
+	// construct an instance of Headroom, passing the element
+	var headroom  = new Headroom(header);
+	// initialise
+	headroom.init();
+
+	$('#image-grid').magnificPopup({
+		type: 'image',
+		delegate: 'a',
+		gallery:{
+			enabled:true
+		}
+	});
+
+	$('.image-grid').each(function() { // the containers for all your galleries
+		$(this).magnificPopup({
+			delegate: 'a', // the selector for gallery item
+			type: 'image',
+			gallery: {
+				enabled:true
+			}
+		});
+	});
+
+	// Menu
+	$('#menu-items li').click(function () {
+		$('#menu-items li').removeClass('pure-menu-selected');
+		$(this).addClass('pure-menu-selected');
+		$(header).removeClass('headroom--unpinned');
+	});
+});
