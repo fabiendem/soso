803b0b82f14e7d81b290c2cf479a3947b38f9641
Fabien Demangeat <fabien.demangeat@gmail.com>
2015-02-05 22:48:41 +0000

    Improve parallax effect, now 60FPS

--- js/main.js
+++ js/main.js
@@ -60,30 +60,120 @@ $(document).ready(function() {
         duration: 600
     });
 
-    var $body = $('body');
+    _now = Date.now || function() {
+        return new Date().getTime();
+    };
+
+    _throttle = function(func, wait, options) {
+        var context, args, result;
+        var timeout = null;
+        var previous = 0;
+        if (!options) options = {};
+        var later = function() {
+            previous = options.leading === false ? 0 : _now();
+            timeout = null;
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
+        };
+        return function() {
+            var now = _now();
+            if (!previous && options.leading === false) previous = now;
+            var remaining = wait - (now - previous);
+            context = this;
+            args = arguments;
+            if (remaining <= 0 || remaining > wait) {
+                clearTimeout(timeout);
+                timeout = null;
+                previous = now;
+                result = func.apply(context, args);
+                if (!timeout) context = args = null;
+            } else if (!timeout && options.trailing !== false) {
+                timeout = setTimeout(later, remaining);
+            }
+            return result;
+        };
+    };
+
+    var vendorTransforms = [
+        '',
+        '-webkit-',
+        '-moz-',
+        '-ms-',
+        '-o-'
+    ];
+
+    var _scale = function($element, ratio) {
+        $.each(vendorTransforms, function(index, vendor) {
+            $element.css(vendor + 'transform', 'scale3d(' + ratio + ', ' + ratio + ', 1)');
+        });
+    };
+
+    var _translateX = function($element, n) {
+        $.each(vendorTransforms, function(index, vendor) {
+            $element.css(vendor + 'transform', 'translate3d(' + n + ', 0, 0)');
+        });
+    };
+
+    var _translateY = function($element, n) {
+        $.each(vendorTransforms, function(index, vendor) {
+            $element.css(vendor + 'transform', 'translate3d(0, ' + n + ', 0)');
+        });
+    };
+
+    var $doodleImage = $('#doodle-image');
     var doodleHeight = $('#doodle').height();
-    var $stickyHeader = $('#sticky-header');    
+    var $stickyHeader = $('#sticky-header');
     var $cloudLeft = $('#cloud-left');
     var $cloudRight = $('#cloud-right');
     var initialHorizontalPosition = 19; // %
     var scrollTopBody;
     var ratioDoodleScroll;
-    var position;
-    $(document).scroll(function () {
-        scrollTopBody = $body.scrollTop();
+    var percentDoodleScroll;
+    var positionCloud;
+    var positionStickyHeader;
+    var scaleDoodle;
+    var applyParallax = function(scrollTopBody) {
         ratioDoodleScroll = 1 + (scrollTopBody - doodleHeight) / doodleHeight;
-        position = initialHorizontalPosition - (initialHorizontalPosition * ratioDoodleScroll) + '%';
+        // Round with 4 decimals
+        ratioDoodleScroll = +ratioDoodleScroll.toFixed(4);
 
-        $cloudLeft.css('left', position);
-        $cloudRight.css('right', position);
+        percentDoodleScroll = ratioDoodleScroll * 100;
 
-        if(scrollTopBody > doodleHeight) {
-            $stickyHeader.show();
+        // Move the clouds
+        _translateX($cloudLeft, -percentDoodleScroll + '%');
+        _translateX($cloudRight, percentDoodleScroll + '%');
+
+        // Scale down the doodle
+        scaleDoodle = 1 - ratioDoodleScroll;
+        scaleDoodle = scaleDoodle < 0 ? 0 : scaleDoodle;
+        scaleDoodle = scaleDoodle > 1 ? 1 : scaleDoodle;
+        _scale($doodleImage, scaleDoodle);
+
+        // Dropdown the filters
+        positionStickyHeader = percentDoodleScroll;
+        positionStickyHeader = positionStickyHeader > 100 ? 100 : positionStickyHeader;
+        _translateY($stickyHeader, positionStickyHeader + '%');
+    };
+
+    var $body = $('body');
+    var isScrolling = false;
+    var AnimationFrame = window.AnimationFrame;
+    AnimationFrame.shim();
+    $(window).scroll(_throttle(function () {
+        isScrolling = true;
+        scrollTopBody = $body.scrollTop();
+        refreshOnScroll(scrollTopBody);
+    }, 60));
+
+    var refreshOnScroll = function(scrollTopBody) {
+        if(isScrolling) {
+            applyParallax(scrollTopBody);
+            AnimationFrame(function() {
+                refreshOnScroll(scrollTopBody);
+            });
+            isScrolling = false;
         }
-        else {
-            $stickyHeader.hide();
-        }
-    });
+    };
 
     var $imageGrid = $('#image-grid');
     $imageGrid.shuffle({
@@ -105,16 +195,6 @@ $(document).ready(function() {
             }
 
             $this.toggleClass('active');
-
-            // Synchronize all the buttons, normal and sticky headers
-            var $otherButtons = $filterOptions.children('[data-group="' + group + '"]');
-            console.log($otherButtons.length);
-            $otherButtons.each(function() {
-                var $button = $(this);
-                if(! isActive && ! $button.hasClass('active')) {
-                    $button.addClass('active');
-                }
-            });
 
             // Filter elements
             $imageGrid.shuffle( 'shuffle', group );
